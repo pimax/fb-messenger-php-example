e3936c5fc5a7b0c319c3b2d1f1ebe9e02b855ac1
pimax <m.pinyugin@gmail.com>
2016-08-11 03:41:59 +0300

    local image

--- index.php
+++ index.php
@@ -28,7 +28,10 @@ $bot = new FbBotApp($token);
 
 if (!empty($_REQUEST['local'])) {
 
-    $message =new ImageMessage(1585388421775947, dirname(__FILE__).'/fb4d_logo-2x.png');
+    $message = new ImageMessage(1585388421775947, dirname(__FILE__).'/fb4d_logo-2x.png');
+
+    $message_data = $message->getData();
+    $message_data['message']['attachment']['payload']['url'] = 'fb4d_logo-2x.png';
 
         echo '<pre>', print_r($message->getData()), '</pre>';
 
