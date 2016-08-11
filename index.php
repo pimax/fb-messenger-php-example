6def4c8434e55ab5483721ec1f61b6bbc793cb0d
pimax <m.pinyugin@gmail.com>
2016-08-11 03:16:04 +0300

    add persistent menu example

--- index.php
+++ index.php
@@ -26,12 +26,6 @@ use pimax\Messages\Adjustment;
 // Make Bot Instance
 $bot = new FbBotApp($token);
 
-if (!empty($_REQUEST['menu'])) {
-    $res = $bot->deletePersistentMenu();
-
-    echo '<pre>', print_r($res), '</pre>';
-}
-
 // Receive something
 if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) {
 
